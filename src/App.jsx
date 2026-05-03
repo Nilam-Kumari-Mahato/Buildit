72e5eac315fa9b4b9aa27f372a3a284fdb7335ae
Nilam Kumari Mahato <nilamkumarimahato10@gmail.com>
2026-05-03 23:19:21 +0530

    all the files and folders has been added

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+import { BrowserRouter , Routes , Route } from "react-router-dom";
+
+import Landing from "./pages/Landing";
+import Validate from "./pages/Validate";
+import Results from "./pages/Results";
+import History from "./pages/History";
+import Navbar from "./components/Navbar";
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <section id="center">
-        <h1>hello, world!</h1>
-      </section>
-
-      
-    </>
+  return( 
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/validate" element={<Validate />} />
+        <Route path="/results" element={<Results />} />
+        <Route path="/history" element={<History />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
---
+++ src/pages/Validate.jsx
@@ -0,0 +1,7 @@
+export default function Validate() {
+    return(
+        <div>
+            
+        </div>
+    )
+}
