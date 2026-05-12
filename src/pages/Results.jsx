414b37c90e6494ba81420a253cac59a716fc1c50
Nilam Kumari Mahato <nilamkumarimahato10@gmail.com>
2026-05-12 23:58:22 +0530

    added the title card

--- src/pages/Results.jsx
+++ src/pages/Results.jsx
@@ -39,14 +39,24 @@ export default function Results() {
   return (
     <div className="background min-h-screen w-full overflow-hidden">
 
-      {/* ORBS */}
-      <div className="orb orb1" />
-      <div className="orb orb2" />
-      <div className="orb orb3" />
-      <div className="orb orb4" />
 
       {/* CONTENT */}
       <div className="relative z-10 max-w-4xl mx-auto px-6 pt-28 pb-20">
+
+        <div className="glass w-full m-3 px-4 py-8">
+            <p className="font-semibold text-m text-[#00ced1] mb-2">Validation Report</p>
+            <div className="flex flex-row gap-4 py-2">
+                <p className="md:text-3xl font-bold text-[#f5f5f5] ">{result.title}</p>
+                <span className="flex items-center text-sm font-light text-[#00ced1] bg-[#004c6d] rounded-full pl-2 pr-2">{result.stage}</span>
+            </div>
+            <p className="text-lg font-semibold text-[#c0c0c0] ">{result.summary.industry}</p>
+
+            <div className="mt-3 flex flex-row gap-7 ">
+                <button className=" py-1 px-2 rounded-lg border border-[#c0c0c0] cursor-pointer text-[#f5f5f5]">Share</button>
+                <button className=" py-1 px-2 rounded-lg border border-[#c0c0c0] cursor-pointer text-[#f5f5f5]">Download pdf</button>
+            </div>
+
+        </div>
 
         {/* We'll build each section here one by one */}
         <p className="text-white">Report loaded! ID: {id}</p>
